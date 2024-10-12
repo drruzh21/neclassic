ef2bf64fda8fb438c3f36c2e7c06ed1bcf6faa8a
egor <egordr0433@gmail.com>
2024-10-12 16:51:58 +0300

    json handler

--- First_step_creating_parameters/json_handler/__init__.py
+++
@@ -1,7 +0,0 @@
-from json_handler.json_handler import JsonHandler
-from json_handler.string_handler import StringHandler
-
-__all__ = [
-    "JsonHandler",
-    "StringHandler"
-]
--- First_step_creating_parameters/json_handler/string_handler.py
+++
@@ -1,8 +0,0 @@
-from json import loads
-
-from json_handler import JsonHandler
-
-
-class StringHandler(JsonHandler):
-    def __init__(self, data: list[str]):
-        super().__init__([loads(jsonstring) for jsonstring in data])
